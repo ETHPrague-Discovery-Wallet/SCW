c2a04fc70a03a96a50eb07dda252fe2e09fed087
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-11 10:32:38 +0200

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,7 @@ import '@typechain/hardhat';
 import { HardhatUserConfig } from 'hardhat/types';
 import * as fs from 'fs';
 import '@nomiclabs/hardhat-etherscan';
+import "hardhat-gas-reporter";
 
 
 require('dotenv').config();
@@ -18,10 +19,18 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8545"
     },
-  sepolia: {
-      url: "https://rpc.sepolia.org",
-      accounts: [process.env.PRIVATE_KEY]
-  },
+    sepolia: {
+        url: "https://rpc.sepolia.org",
+        accounts: [process.env.MNEMONIC]
+    },
+    optimism: {
+      url: "https://optimism-mainnet.infura.io/v3/"+process.env.INFURA_KEY,
+      accounts: [process.env.MNEMONIC]
+    },
+    scroll: {
+      url: 'https://alpha-rpc.scroll.io/l2',
+      accounts: [process.env.MNEMONIC]
+    }
   },
   solidity: {
     version: "0.8.12",
